e9055857b8b23b64cbd47d7d2fd27ce1d9777902
duyhieu2509 <buihieuduya4@gmail.com>
2024-12-26 22:56:49 +0700

    asd

--- RichLife-Topic/src/main/java/com/richlife/topic/RichLife_Pharma/RichLifePharmaApplication.java
+++ RichLife-Topic/src/main/java/com/richlife/topic/RichLife_Pharma/RichLifePharmaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RichLifePharmaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RichLifePharmaApplication.class, args);
 	}
-
 }
